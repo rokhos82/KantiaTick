bdd79a22d203ebf69b45eacd5f69e986fbcc22c2
Justin Lane <justin.l.lane@gmail.com>
2013-12-09 16:09:25 -0700

    localStorage works

--- tick.js
+++ tick.js
@@ -3,16 +3,27 @@ var tick = {};
 tick.player = {};
 tick.player.ui = {};
 tick.player.ui.initiative = undefined;
+tick.player.ui.reactionSpeed = undefined;
 tick.player.name = undefined;
 tick.player.initiative = undefined;
+tick.player.reactionSpeed = 5;
 tick.player.getInitiative = function() {
 	tick.player.initiative = prompt("Initiative");
 	tick.player.ui.initiative.innerHTML = "Initiative: " + tick.player.initiative;
+	tick.local.obj.initiative = tick.player.initiative;
+	localStorage[tick.local.key] = JSON.stringify(tick.local.obj);
 };
+tick.player.getReactionSpeed = function() {
+	tick.player.reactionSpeed = prompt("Reaction Speed");
+	tick.player.ui.reactionSpeed.innerHTML = "Reaction Speed: " + tick.player.reactionSpeed;
+	tick.local.obj.reactionSpeed = tick.player.reactionSpeed;
+	localStorage[tick.local.key] = JSON.stringify(tick.local.obj);
+}
 
 tick.changeMode = function() {
 	tick.reset();
 	delete localStorage[tick.local.key];
+	tick.local.obj = undefined;
 	tick.initialize();
 };
 
@@ -35,7 +46,6 @@ tick.playerMode = function() {
 	var h = document.createElement("h1");
 	h.appendChild(t);
 	tick.root.appendChild(h);
-	localStorage[tick.local.key] = JSON.stringify({mode: "pc"});
 
 	var button = document.createElement("button");
 	var t = document.createTextNode("Change Mode");
@@ -49,13 +59,27 @@ tick.playerMode = function() {
 	button.appendChild(t);
 	tick.root.appendChild(button);
 
+	var button = document.createElement("button");
+	var t = document.createTextNode("Reaction Speed");
+	button.onclick = tick.player.getReactionSpeed;
+	button.appendChild(t);
+	tick.root.appendChild(button);
+
 	var d = document.createElement("div");
 	tick.root.appendChild(d);
 	var span = document.createElement("span");
-	var t = document.createTextNode("Initiative:");
+	tick.player.initiative = tick.local.obj.initiative ? tick.local.obj.initiative : "";
+	var t = document.createTextNode("Initiative: " + tick.player.initiative);
 	span.appendChild(t);
 	d.appendChild(span);
 	tick.player.ui.initiative = span;
+
+	var span = document.createElement("span");
+	tick.player.reactionSpeed = tick.local.obj.reactionSpeed ? tick.local.obj.reactionSpeed : 5;
+	var t = document.createTextNode("Reaction Speed: " + tick.player.reactionSpeed);
+	span.appendChild(t);
+	d.appendChild(span);
+	tick.player.ui.reactionSpeed = span;
 };
 
 tick.mode = null;
@@ -72,16 +96,19 @@ tick.modes = {
 
 tick.local = {};
 tick.local.key = "kantiatick";
+tick.local.obj = undefined;
 
 tick.root = null;
 
 tick.initialize = function(root) {	
 	tick.root = document.getElementById("root");
 	tick.reset();
+	tick.local.obj = {};
 
 	if(localStorage[tick.local.key]) {
 		var j = localStorage[tick.local.key];
 		var obj = JSON.parse(j);
+		tick.local.obj = obj;
 		tick.modes[obj.mode].func();
 	}
 	else {
@@ -122,5 +149,7 @@ tick.setMode = function(select) {
 	var m = select.selectedOptions[0].value;
 	tick.mode = m;
 	tick.reset();
+	tick.local.obj.mode = m;
+	localStorage[tick.local.key] = JSON.stringify(tick.local.obj);
 	tick.modes[m].func();
 };
